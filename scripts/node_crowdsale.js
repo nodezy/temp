2e4c1b52e55d9adff7cdf9437592ae1a66651e90
nodezy <80292753+nodezy@users.noreply.github.com>
2021-04-13 03:33:21 -0400

    Update node_crowdsale.js

--- scripts/node_crowdsale.js
+++ scripts/node_crowdsale.js
@@ -3,12 +3,12 @@ const TOKEN_ABI = require('../src/abis/abi.json');
 
 var theToken = "0x8bb52c2792e852aF8855c7cA617EDCB0FEdA3d85"  //token contract
 var theCrowdSale = "0x4A4C8E4C0D1B898b8Aae9CCc9A8f69dED627EC6e" //crowdsale address
-var theAmount = "10000000000000000000000000000000000000000000000000000000" //1 Trillion
+var theAmount = "1000000000000000000000000000000" //1 Trillion
  
-var keystore = "ad3d92f8ffc69deb33b25456f2c01ea01ba4b8a6b02c385d30f87e620993913b" //private key of wallet holding tokens
+var keystore = "" //private key of wallet holding tokens
 
-const INFURA_URL = "https://ropsten.infura.io/v3/21d7ff87ab3941cf8cfa4c0cb379a384"  //local eth node rpc
-const INFURA_WS_URL = "wss://ropsten.infura.io/ws/v3/21d7ff87ab3941cf8cfa4c0cb379a384" //local eth node websocket
+const INFURA_URL = ""  //local eth node rpc
+const INFURA_WS_URL = "" //local eth node websocket
 
 const web3 = new Web3(new Web3.providers.HttpProvider(INFURA_URL));
 
